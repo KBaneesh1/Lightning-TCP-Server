895bbad340b2a7024353d18c0071c2c8d2eec086
AKSHAR-0909 <112763290+AKSHAR-0909@users.noreply.github.com>
2024-05-07 13:14:15 +0530

    GUI

---
+++ src/parallel/client/client.cpp
@@ -0,0 +1,185 @@
+#include <iostream>
+#include <fstream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <limits>
+#include <gtkmm.h>
+
+using namespace std;
+
+class MainWindow : public Gtk::Window
+{
+public:
+    MainWindow();
+
+protected:
+    Gtk::Box mainBox;
+    Gtk::Box fileExplorerBox;
+    Gtk::TreeView fileTreeView;
+    Gtk::ScrolledWindow fileScrolledWindow;
+    Gtk::TextView textEditorTextView;
+    Glib::RefPtr<Gtk::TextBuffer> textBuffer;
+    Glib::RefPtr<Gtk::TreeStore> fileTreeStore;
+    struct Columns : public Gtk::TreeModel::ColumnRecord
+    {
+        Columns()
+        {
+            add(filename);
+        }
+
+        Gtk::TreeModelColumn<std::string> filename;
+    } columns;
+
+    int clientSocket;
+
+    void onFileSelectionChanged();
+
+    void loadFile(const std::string& filePath);
+
+    void connectToServer();
+
+    public:
+    void populateFileExplorer(const std::string& directoryPath);
+};
+
+MainWindow::MainWindow() : clientSocket(-1)
+{
+    set_title("GOOGLE DOCS");
+    set_default_size(800, 600);
+
+    // Set up the main box
+    set_child(mainBox);
+
+    // Set up the file explorer pane
+    fileTreeStore = Gtk::TreeStore::create(columns);
+    fileTreeView.set_model(fileTreeStore);
+    fileTreeView.append_column("Files", columns.filename);
+
+    // Set up the scrolled window
+    fileScrolledWindow.set_child(fileTreeView);
+    fileScrolledWindow.set_size_request(250, -1); // Set the width to 250 pixels
+
+    fileExplorerBox.append(fileScrolledWindow);
+    mainBox.append(fileExplorerBox);
+
+    // Set up the text editor pane
+    textBuffer = Gtk::TextBuffer::create();
+    textEditorTextView.set_buffer(textBuffer);
+    textEditorTextView.set_size_request(550,1);
+    mainBox.append(textEditorTextView);
+
+    // Connect signals
+    fileTreeView.signal_cursor_changed().connect(sigc::mem_fun(*this, &MainWindow::onFileSelectionChanged));
+
+    // Connect to server
+    connectToServer();
+}
+
+void MainWindow::populateFileExplorer(const std::string& directoryPath)
+{
+    if (clientSocket < 0) {
+        cerr << "Error: Not connected to server\n";
+        return;
+    }
+
+    // Send request to server to get list of files
+    const char *message = "GET_FILES\n";
+    int bytesSent = send(clientSocket, message, strlen(message), 0);
+    if (bytesSent < 0) {
+        cerr << "Error: Failed to send data to server\n";
+        return;
+    }
+
+    // Receive file list from server
+    char buffer[1024] = {0};
+    int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesRead < 0) {
+        cerr << "Error: Failed to receive data from server\n";
+        return;
+    }
+
+    // Update file tree view with received file list
+    std::string fileData(buffer, bytesRead);
+    std::istringstream iss(fileData);
+    std::string filename;
+    while (std::getline(iss, filename, '\n')) {
+        Gtk::TreeModel::Row row = *(fileTreeStore->append());
+        row[columns.filename] = filename;
+    }
+
+    show();
+}
+
+void MainWindow::onFileSelectionChanged()
+{
+    Gtk::TreeModel::iterator iter = fileTreeView.get_selection()->get_selected();
+    if (iter)
+    {
+        Gtk::TreeModel::Row row = *iter;
+        std::string filename = row[columns.filename];
+        std::string filePath = filename;
+        loadFile(filePath);
+    }
+}
+
+void MainWindow::loadFile(const std::string& filePath)
+{
+    if (clientSocket < 0) {
+        cerr << "Error: Not connected to server\n";
+        return;
+    }
+
+    // Send request to server to get file content
+    std::cout<<filePath<<std::endl;
+    std::string message = "GET_FILE\n" + filePath + "\n";
+    int bytesSent = send(clientSocket, message.c_str(), message.size(), 0);
+    if (bytesSent < 0) {
+        cerr << "Error: Failed to send data to server\n";
+        return;
+    }
+
+    // Receive file content from server
+    char buffer[1024] = {0};
+    int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesRead < 0) {
+        cerr << "Error: Failed to receive data from server\n";
+        return;
+    }
+
+    // Update text buffer with received file content
+    std::string content(buffer, bytesRead);
+    textBuffer->set_text(content);
+}
+
+void MainWindow::connectToServer()
+{
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket < 0) {
+        cerr << "Error: Failed to create socket\n";
+        return;
+    }
+
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+        cerr << "Error: Failed to connect to server\n";
+        // close(clientSocket);
+        return;
+    }
+
+    populateFileExplorer("./");
+}
+
+int main(int argc, char* argv[])
+{
+    auto app = Gtk::Application::create("org.gtkmm.examples.base");
+    MainWindow mainWindow;
+    // mainWindow.connectToServer();
+    return app->make_window_and_run<MainWindow>(argc, argv);
+}
---
+++ src/parallel/server/main.cpp
@@ -0,0 +1,254 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <cstring>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <unistd.h>
+#include <fstream>
+#include<dirent.h>
+#include <sstream> 
+
+
+
+//FINAL
+using namespace std;
+
+pthread_mutex_t dbMutex;
+pthread_mutex_t qMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
+
+const int threadCount = 10;
+queue<int> clients;
+vector<int> editClients;
+
+class Database {
+    private:
+    unordered_map<string, string> db;
+
+    public:
+    string write(string key, string value) {
+        pthread_mutex_lock(&dbMutex);
+        db[key] = value;
+        pthread_mutex_unlock(&dbMutex);
+        return "FIN";
+    }
+
+    string read(string key) {
+        unordered_map<string, string>::iterator iter = db.find(key);
+        if (iter == db.end()) return "NULL";
+        return iter->second;
+    }
+
+    int count() {
+        return db.size();
+    }
+
+    string remove(string key) {
+        // cout<<key<<endl;
+        pthread_mutex_lock(&dbMutex);
+        if(db.find(key) != db.end()) {
+        cout<<db[key]<<endl;
+        bool done = db.erase(key);
+        pthread_mutex_unlock(&dbMutex);
+        return "FIN";
+      }
+   
+        pthread_mutex_unlock(&dbMutex);
+        return "NULL";
+    
+    }
+};
+
+Database db;
+
+int CreateSocket(int portno) {
+    int sfd = socket(AF_INET, SOCK_STREAM, 0);
+    int iSetOption = 1;
+    setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, (char*)&iSetOption,sizeof(iSetOption));
+
+    if (sfd == -1) {
+        // perror("socket");
+        return -1;
+    }
+
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(portno);
+
+    if (bind(sfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        // perror("bind");
+        close(sfd);
+        return -1;
+    }
+
+    if (listen(sfd, 5) == -1) {
+        // perror("listen");
+        close(sfd);
+        return -1;
+    }
+
+    return sfd;
+}
+
+int AcceptConnection(int serverSocket) {
+    struct sockaddr_in clientAddress;
+    socklen_t clientAddressLen = sizeof(clientAddress);
+    int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLen);
+    if (clientSocket == -1) {
+        // perror("accept");
+        return -1;
+    }
+    return clientSocket;
+}
+
+string listTextFiles(const string& directoryPath) {
+    string fileList;
+
+    // Open directory
+    DIR* dir = opendir(directoryPath.c_str());
+    if (dir == nullptr) {
+        cerr << "Error: Unable to open directory " << directoryPath << endl;
+        return fileList;
+    }
+
+    // Read directory entries
+    dirent* entry;
+    while ((entry = readdir(dir)) != nullptr) {
+        string filename = entry->d_name;
+        if (filename != "." && filename != "..") {
+            if (filename.size() > 4 && filename.substr(filename.size() - 4) == ".txt") {
+                fileList += filename + "\n";
+            }
+        }
+    }
+
+    cout<<fileList<<endl;;
+    // Close directory
+    closedir(dir);
+
+    return fileList;
+}
+
+
+void* HandleClient(void* arg) {
+
+    int clientSocket = *((int*)arg);
+    char buffer[1024];
+    ssize_t bytesReceived;
+
+    while(1){
+        bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytesReceived == -1) {
+            close(clientSocket);
+            return NULL;
+        }
+
+        string temp = "";
+        vector<string> msg;
+        for (int i = 0; i < bytesReceived; i++) {
+            cout<<temp<<endl;
+            if (buffer[i] != '\n') temp += buffer[i];
+            else {
+                msg.push_back(temp);
+                temp = "";
+            }
+        }
+
+        int i = 0;
+        int len = msg.size();
+        // cout<<len<<endl;
+        while (i < len) {
+            if(msg[i]=="GET_FILES"){
+                string response=listTextFiles("./server/text_files/");
+                send(clientSocket,response.c_str(),response.size(),0);
+            }
+            if(msg[i]=="GET_FILE"){
+                string filename="./server/text_files/";
+                filename=(i+1)<len?(filename+msg[++i]):"NULL";
+                ifstream file(filename);
+                string fileContents;
+
+                stringstream buffer;
+                buffer << file.rdbuf();
+                fileContents = buffer.str();
+                file.close();
+
+                send(clientSocket, fileContents.c_str(), fileContents.size(), 0);
+
+            }
+            if (msg[i] == "END") break;
+
+            i++;
+        }
+        string result = "\n";
+        cout<<result<<endl;
+        send(clientSocket, result.c_str(), result.size(), 0);
+    }
+    close(clientSocket);
+}
+
+void* HandleQueue(void* arg) {
+    while (1) {
+        pthread_mutex_lock(&qMutex);
+        int clientSocket;
+        if (clients.empty()) {
+            pthread_cond_wait(&cv, &qMutex);
+            if (!clients.empty()) {
+                clientSocket = clients.front();
+                clients.pop();
+            }
+        }
+        pthread_mutex_unlock(&qMutex);
+        HandleClient(&clientSocket);
+    }
+}
+
+void* AcceptConnections(void* arg) {
+    int serverSocket = *((int*)arg);
+    while (1) {
+        int clientSocket = AcceptConnection(serverSocket);
+        pthread_mutex_lock(&qMutex);
+        clients.push(clientSocket);
+        pthread_cond_signal(&cv);
+        pthread_mutex_unlock(&qMutex);
+    }   
+}
+
+int main(int argc, char** argv) {
+    int portno;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    portno = atoi(argv[1]);
+
+    pthread_t threadPool[threadCount];
+
+    for (int i = 0; i < threadCount; i++) {
+        pthread_create(&threadPool[i], NULL, HandleQueue, NULL);
+    }
+
+    int serverSocket = CreateSocket(portno);
+
+    pthread_t acceptThread;
+    pthread_create(&acceptThread, NULL, AcceptConnections, (void*)&serverSocket);
+
+    for (int i = 0; i < threadCount; i++) {
+        pthread_join(threadPool[i], NULL);
+    }
+
+    pthread_join(acceptThread, NULL);
+
+    close(serverSocket);
+
+    return 0;
+}
